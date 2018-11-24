60f216a588d9071a0be74782f5b3773260ca01d6
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-11-24 15:43:43 -0400

    Add project files.

---
+++ ludorill-server-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ludorill_server_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
